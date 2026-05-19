984c6a191314cff5d94caf44494d3537aa2d741d
Anarohnd <anarohnd@gmail.com>
2026-05-19 12:57:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -33,33 +33,21 @@ wss.on('connection', (ws) => {
     };
     
     let zelloToken;
-    try {
-        zelloToken = jwt.sign(payload, PRIVATE_KEY, { algorithm: 'RS256' });
-    } catch (err) {
-        console.error('Error al firmar el token con la Private Key: ' + err.message);
-        ws.close();
-        return;
-    }
+try {
+    const llaveDirecta =`-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1432B7cBNYTQhiQBOAZc
+b+HCC8HRs6ppRvJSpc/7v5FGa6Fx9Eji6IwrMnIQWAMrBZ7CBtmkQXfD6Jj3Jvux
+ylQcHXsa4mPAdgd3Iz/0IkSAEnTQsk61S48ROtDIk2Lahbe53dMmcjxPDNDtKCmw
+pXAUEf4fAOFNp7E0VAFHXcnvKTuWzZjo0+LhZwSOoNuGcV1qvbJWXMIFBFUanMjo
+A1wO0U+PN7d0rvMZcxEFirA++36d3Vw3QM/XVPC8FblfFXpwLui/yUT+dlxidDnp
+ZgnO+YxVGpPXogpbrY+S2tc8PEonAWpwajLxJLBdfmz7LAJswa/fmGvDtDfvS58w
+HQIDAQAB
+-----END PUBLIC KEY-----`
+;
 
-    const zelloWs = new WebSocket('wss://zello.page/api/v1/stream?token=' + zelloToken);
-
-    zelloWs.on('open', () => {
-        console.log('Conexión exitosa con la API de Zello');
-    });
-
-    zelloWs.on('message', (data) => {
-        if (ws.readyState === WebSocket.OPEN) {
-            ws.send(data);
-        }
-    });
-
-    zelloWs.on('close', () => {
-        console.log('Conexión con Zello cerrada');
-        ws.close();
-    });
-
-    ws.on('close', () => {
-        console.log('Cliente de Blogger desconectado');
-        zelloWs.close();
-    });
-});
+    zelloToken = jwt.sign(payload, llaveDirecta, { algorithm: 'RS256' });
+} catch (err) {
+    console.error('Error al firmar el token con la Private Key: ' + err.message);
+    ws.close();
+    return;
+}
