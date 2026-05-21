559c6a6d7b29c65ac8d81de804e068f331ca914d
Anarohnd <anarohnd@gmail.com>
2026-05-20 22:45:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,34 @@ app.get('/get-token', (req, res) => {
     };
 
     try {
-        const base64Key = process.env.ZELLO_PRIVATE_KEY || '';
+        const base64Key = process.env.ZELLO_PRIVATE_KEY || -----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDUaGpQeKwUlTV8
+d3VtMC3HXUmenoxwpQx+KvhXrFY99tbu+W7Ch1tTObvmPjWkq0TfL17jRstZczlt
++aCDOl1T1bMpAoHqhSOoLMPF1CC3JrDnmpVUnTEQguGMFrs92sMuXxFCNf4dQJ/r
+P+QwvSJSz7yyvDDx5wpX4xlPRwJ9kb+wrhfufJVg9ejhcxegw+ns51GSCG66wGQD
+Sddu0DoPnJmvLAtFRDNr7dipBbZbezST4dfQbXHsoTgd//zkVBVo25xrcBeGGcTb
+cdNbCGrr9nHaPtvzJCaFIg+A+NjTDiaKPHZZODVWAHt1PG5YUycxgrfnSmONTc4L
+6QDbK4nnAgMBAAECggEARgNM182O2xH7mVU+7Yit7kL/mzsji5W7snKzfSKfIrmU
+Sx0ItzIqlho3p0LmJ6sUfoR8wGl7abajPW7Ey7yXOqT78w6SiBzujeaDeVfcmMI2
+GYFIq6VMoOajNavgeBj9sPFBHRPy+O1uSPq8WpxdDfLPfhKuB25iaGnA0/LSDVU9
+hhtfZ1/nvxWA8rM+/6AVIWhINxf4ScJa2f67R8iiDI8MO/GpuGNSwLorUxnekD2G
+2vpfvFG1qQ37uo3vg9xzBtzG4jhPvLU/tATxeDIZ8YvEYuRpO8upjREc6sPgqaS8
+feR3m46gzKVjWuDto5R11RbJGlNvOGPyah/WaSmo8QKBgQDyJY1eVIGFl5sytg/m
+Zz/1eXpUnetYOrnShBGVtzLcrHg+SXmwsQTSw1RX9l1CoGzHL840P5/6Ae/oxpJC
+q/N2xs+0TcTLxFpyHXpmziG4kprvrZnIxLVj/LrH9tk1XgsKfOZrprMjNtYiMnvI
+HEj0SIflkeaORczdfCBM4r7DUwKBgQDgj0/3yQ9iX+Q6NUrauj/N2OAKKac0M4iE
+OE9Nm/rSYzM7b5TZc+H01v8KFJop/nZegBndLQCLoJsdtEV7P2rNAFVcVhaX2ayY
+GfHD8rRIK6QDIGG2uCbJQWLGvzqwTxjLe7Phycq5rSKfOXVLzqf8nYHAhUK6POtB
+T9WXHSNAnQKBgF5kKw5pbK/x4ErNU7dh6jm7Z+dSQ+p5wfQBDYXKCyagiKLnK2uo
+JZQvFlLPs50EQkMHLJ+LacYgOTAJL/yTYO2dJFxJsHwqB4PSX+NxWQ3FAGaLypXL
+sFXQr9LBM/yssysccwnlypDgkGp2OXBRLlMjbu4biL9PEEQKXeD6xpgHAoGBALxr
+w7+3tu7mYwvsV4i8KnlAdoxQcvm8G9pFnVUTghwyHHTMpWrobzoPhCQjMU/3Mb8N
+YaZR8lk2Q7Fi58IIbkbg9r7phT+Y59cgPdoateawp4fwDc1RK0pHUOhO4KG7XKSw
+gTj9JtsxSRGtFpyrJA005AB0UI8QOp2srR2E12WJAoGBAL85lCXMzMfMbqDJGWqR
+zWotqeQxLsutBR+fPlE3gqsx83MH5B6Qh3aJVQT26csguKQKE7Zw1fJBTH+HW+J6
+C1aare69KLFMU8hpmMnznBfKHWaap5uY0mF/3AmMEaC9TcUotXJLbdbj2nutOmBj
+IW6KcZggQosUR84S34BLqsoP
+-----END PRIVATE KEY----- ;
         const privateKey = Buffer.from(base64Key, 'base64').toString('utf8');
         
         const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
